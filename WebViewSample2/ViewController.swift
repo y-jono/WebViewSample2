e9fd38507157caf918d69968ebe70ac4846d2c66
y_jono <pek9dkl@gmail.com>
2020-05-22 22:57:26 +0900

    Feature: ButtonクリックでWebView内のNuxt.jsアプリをRouterで遷移させることに成功した。

--- WebViewSample2/ViewController.swift
+++ WebViewSample2/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet
+    weak var webView: WKWebView!
+    
+    @IBOutlet
+    weak var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.webView.navigationDelegate = self
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        self.webView.load(URLRequest(url: URL(string: "http://192.168.0.224:3000/")!))
+    }
+    
+    @IBAction func onTouchUpInside(_ sender: Any) {
+        let execJsFunc = "$nuxt.$router.push('/inspire')"
+        
+        self.webView.evaluateJavaScript(execJsFunc, completionHandler: nil)
+    }
 }
 
+
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+    }
+}
