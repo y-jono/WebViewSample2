1757d391cda37d4df7d6440bddaba663719e1a0f
y_jono <pek9dkl@gmail.com>
2020-05-22 22:11:05 +0900

    Initial Commit

---
+++ WebViewSample2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebViewSample2
+//
+//  Created by Seino Yoshinori on 2020/05/22.
+//  Copyright © 2020 yoshinori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
